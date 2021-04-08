86e8d79e89d09428d1df18af43271fde0785cb01
adnankhan1007 <68551903+adnankhan1007@users.noreply.github.com>
2021-04-08 12:36:44 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+<link rel="stylesheet" type="text/css" href="css/navbar.css">
+<link rel="stylesheet" type="text/css" href="css/home.css">
+
+    <title>Basic Banking System</title>
+  </head>
+  <body background="img/bg1.png">
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+    <?php
+include 'navbar.php';
+?>
+<div class="row intro py-9">
+<div class="col-sm-12 col-md">
+<div class="heading text-center my-5">
+
+<h3>VISTA Bank</h3>
+<h1>Welcomes You</h1>
+</div>
+</div>
+</div>
+
+<div class="row activity text-center">
+  <div class="col-md act">
+<img src="img/mk.png"height="270" width="199" class="img-fluid">
+<br>
+<a href="transfermoney.php"><button type="button"class="btn btn-info btn-lg">Make a Transaction</button></a>
+</div>
+<div class="col-md act">
+<img src="img/transaction.png"height="270" width="199" class="img-fluid">
+<br>
+<a href="transactionhistory.php"><button type="button" class="btn btn-info btn-lg">Transaction History</button></a>
+
+</div>
+</div>
+</div>
+<footer class="text-center mt-5 py-4">
+<p>Made by <b>Adnan Mohammad Khan</b> <br>2021</p>
+</footer>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar --> 
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="collapsibleNavbar">
+<ul class="navbar-nav ml-auto">
+<li class="nav-item">
+<a class="nav-link" href="Home.php" style="color: white;" >Home</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="viewallcustomers.php" style="color: white;">View All Users</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="transfermoney.php" style="color:white;">Transfer Money</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="transactionhistory.php" style="color:white;">Transaction History</a>
+</li>
+</div>
+</nav>
